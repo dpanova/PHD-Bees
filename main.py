07c05b97493ec7afd8f0cebb836b3bc4c263337b
VMWAREM\dpanova <dpanova@vmware.com>
2023-09-27 16:18:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+
+a = pd.DataFrame()
+print(9+5)
+print(a)
+
