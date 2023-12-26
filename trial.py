7645a30a2045567f9dd3b8a701b15f486d15087f
Deni <denitsa.panova@gmail.com>
2023-12-26 13:19:18 +0200

    trying the new functions

--- trial.py
+++ trial.py
@@ -1,26 +1,41 @@
 from BeeOrNotToBeeClassification import BeeNotBee
-
+import time
 bee = BeeNotBee()
 # read and validate the annotation data
 bee.read_annotation_csv()
-
+# this should be first to integrate the changes
+bee.acoustic_file_names()
 bee.split_annotation_data()
 
+acc, precision, recall, misclassified = bee.random_forest_results()
+
+#%%
+
+# This was working before and we are aiming at it
+from BeeOrNotToBeeClassification import BeeNotBee
+import time
+bee = BeeNotBee()
+# read and validate the annotation data
+bee.read_annotation_csv()
+# this should be first to integrate the changes
+bee.acoustic_file_names()
+bee.split_annotation_data()
+
+acc, precision, recall, misclassified = bee.random_forest_results()
+
 
 
 #%%
-import librosa
-import numpy as np
-file_name1 = 'data/bee/bee_index1.wav'
-samples1, sample_rate1 = librosa.load(file_name1, sr=None, mono=True, offset = 0.0, duration=None)
-#a = bee.harley_transformation_with_window(samples1)
-#%%
-duration = bee.annotation_df.loc[bee.annotation_df['index']==1,'duration'][0]
-dt = 1/sample_rate1
-t = np.arange(0,duration,dt)
-npnts  = len(t)
-#bee.freq_powerspect_func(x=samples1,dt=dt, npnts=npnts)
-a = bee.binning(x=samples1, dt=dt, npnts = npnts,n_bins='a')
+# TODO
+# 1. check which files had issues and why, we need to document that - it looks like we just haven't split this data at all, so we need to revisit how do we split data at all, maybe after comparing it with the directory ?
+# 4. why don't we take the max in each bin?
+# 5. check if the splitting of the data is correct - annotation df creation
+# 6. we should check the fft part
 
-#%%
-bee_files = os.listdir(os.getcwd())
+#1901 is not correct, we need to investigate why the file is null
+
+# check the duration for the misclassified ones, the hypothesis is that it is the short ones
+# we can also check with different window functions and xgboost
+# we should also time the algorithm
+
+
