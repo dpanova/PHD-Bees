5d7d16de7fb2989188cd52c53442428e0528d01c
Deni <denitsa.panova@gmail.com>
2024-02-24 19:49:06 +0200

    play with the CNN convolution model

--- trial.py
+++ trial.py
@@ -11,78 +11,82 @@ bee.new_y_label_creation()
 bee.acoustic_file_names() # here we need to figure out if we need it and why
 # split the data
 bee.split_annotation_data()
+
+
+
 # augment the data
+#bee.data_augmentation_df()
 
-
+# need to normalize and shorten the augmented data
 
 
 #transform the data
-#x_transformed = bee.data_transformation_df(bee.X_train_index,func = 'mel spec')
+x_transformed = bee.data_transformation_df(bee.X_train_index,func = 'mel spec')
 # train data
 #acc, precision, recall, misclassified = bee.random_forest_results()
+
 #%%
-#TODO absract this piece of code because it is in the
-
-# get the necessary indices to trace files easily
-file_index = bee.X_train_index.iloc[0,0]
-train_index = bee.X_train_index.index[0]
-# this index is necessary to ensure we have the correct file name (coming from the annotation file)
-label = bee.y_train.loc[train_index, bee.y_col]
-# check if the file from the annotation data exists in the folders
-
-# identify the file name
-file_name = [x for x in bee.accoustic_files if x.find('index' + str(file_index) + '.wav') != -1][0]
-file_name = bee.acoustic_folder + file_name
-
-# read the file
-samples, sample_rate = librosa.load(file_name, sr=None, mono=True, offset=0.0, duration=None)
-
-from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, Shift
-augment = Compose([
-    AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
-    TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5),
-    PitchShift(min_semitones=-4, max_semitones=4, p=0.5),
-    Shift(min_shift=-0.5, max_shift=0.5, p=0.5),
-])
-import soundfile as sf
-# Augment/transform/perturb the audio data
-augmented_samples = augment(samples=samples, sample_rate=sample_rate)
-augment_folder = 'data/augment/'
-
-bee.X_augmented_train = pd.DataFrame(columns=[bee.x_col])
-bee.y_augmented_train = pd.DataFrame(columns=[bee.y_col])
-# Do we need this at all?
-# recording = AudioSegment.from_wav('data/' + f)
-augmented_file_index = file_index+10000
-augmented_file_name = 'index' + str(augmented_file_index) + '.wav'
-augmented_train_index = train_index + 10000
-sf.write(augment_folder+'aug1.wav',augmented_samples,sample_rate)
-bee.X_augmented_train.loc[augmented_train_index,bee.x_col] = augmented_file_index
-bee.y_augmented_train.loc[augmented_train_index,bee.y_col] = label
 
 
 
-# TODO
-# not most optimal code
-# clear the folder if there are other files
-# save wav files into a separate folder
-# save the index to another X train and the associated labels into the other y_train
-# number of aumentations ? stratified?
+ # transform the categorical value
+bee.y_train = bee.y_train.astype('category')
+bee.y_train_code = bee.y_train.apply(lambda x: x.cat.codes)
+y = keras.utils.to_categorical(bee.y_train_code, num_classes=3)
 
-
+# transform the X matrix
+features_convolution = np.reshape(x_transformed,(400,128, -1,1))
 
 
 
 #%%
-# import multiprocessing as mp
-# pool = mp.Pool(processes=mp.cpu_count())
-# # TODO once, we add a new function, we need to check if this is working or not
-func = 'mfcc'
-aa= list()
-for train_index, row in bee.X_train_index.iloc[:2,:].iterrows():
-    print(train_index)
-    a= bee.data_transformation_row((train_index, row, func))
-    aa.append(a)
+import multiprocessing as mp
+pool = mp.Pool(processes=mp.cpu_count())
+X_transformed = pool.map(bee.data_transformation_row,[(train_index, row, func) for train_index, row in bee.X_train_index.iterrows()])
+
+#%%
+
+cols = ['train_index','file_index']
+max_length = max([len(x) for x in X_transformed if x is not None])
+cols = cols + ['col' + str(x) for x in range(max_length - 2)]
+# transform to data frame
+X_df = pd.DataFrame(columns=cols)
+for x in X_transformed:
+    if len(x) ==max_length:
+        print(len(X_df))
+        print(len(x))
+        X_df.loc[len(X_df)] = x
+    else:
+        x_updated = x+[None]*(max_length-len(x))
+        X_df.loc[len(X_df)] = x_updated
+
+#%%
+import glob
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+from keras import optimizers
+import keras
+
+# features_convolution = x_transformed.iloc[:,2:]
+features_convolution = np.reshape(x_transformed.iloc[:,2:],(678,398, -1,1))
+#create model
+model = Sequential()#add model layers
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(398,1,1))) #here is the key to the shape
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#TODO update the x_test features
+model.fit((x_transformed.iloc[:,2:], y, validation_data=(X_test, y_test), epochs=3)
+
+
+
 
 #%%
 
@@ -135,4 +139,5 @@ S = np.mean(librosa.feature.melspectrogram(y = samples, sr=sample_rate, n_fft=20
 # check the duration for the misclassified ones, the hypothesis is that it is the short ones
 # we can also check with different window functions and xgboost
 # we should also time the algorithm
-
+# resize to have the same length of samples
+# should we normalize it?
