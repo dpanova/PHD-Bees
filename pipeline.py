81d2d26f8083458c099226b2decee6f9298060d7
Deni <denitsa.panova@gmail.com>
2024-05-02 21:05:50 +0300

    fix pipeline to train hubert

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,6 @@
-from datasets import Audio
-from transformers import AutoModelForAudioClassification, TrainingArguments, Trainer, AutoFeatureExtractor
 
 from BeeData import BeeData
-from BeeClassification import BeeClassification
-
 slice= 5
-
 #Get the data
 beedata = BeeData()
 beedata.annotation_data_creation()
@@ -15,6 +10,7 @@ beedata.split_acoustic_data_sliced()
 beedata.create_validate_data(sliced=True)
 
 #%%
+from BeeClassification import BeeClassification
 beeclass = BeeClassification()
 # read and validate the annotation data
 beeclass.read_annotation_csv()
@@ -29,12 +25,21 @@ beeclass.split_annotation_data()
 beeclass.data_augmentation_df()
 
 #%%
-# RF results with MFCC
+# RF results with MFCC and PCA
 acc_pca, precision_pca, recall_pca, misclassified_pca, pca_variance, forest_importances_pca = beeclass.random_forest_results()
 
 #%%
-
+# RF results with MFCC and NO PCA
 acc, precision, recall, misclassified, forest_importances = beeclass.random_forest_results(do_pca=False)
+
+#%%
+# RF results with MEL SPEC and PCA
+acc_pca_m, precision_pca_m, recall_pca_m, misclassified_pca_m, pca_variance_m, forest_importances_pca_m = beeclass.random_forest_results(func = 'mel spec')
+
+#%%
+# RF results with MEL SPEC and NO PCA
+acc_m, precision_m, recall_m, misclassified_m, forest_importances_m = beeclass.random_forest_results(func='mel spec', do_pca=False)
+
 
 #%%
 #create data dictionary
